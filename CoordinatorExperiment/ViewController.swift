24367d1802a61cadcd917a03843ef4fdd587913f
ahmedkamohamed <ahmed.khaled.a.mohamed@gmail.com>
2018-11-29 11:44:09 +0100

    Initial Commit

---
+++ CoordinatorExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorExperiment
+//
+//  Created by Ahmed Khaled Abdelhameed Mohamed on 29.11.18.
+//  Copyright © 2018 Ahmed Khaled Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
